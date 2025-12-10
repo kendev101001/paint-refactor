95c64c11d20099d970500d3b71693d6f3304b6b7
kendev101001 <ih2d2bf@gmail.com>
2025-12-10 16:33:52 +0000

    minor changes

--- main.py
+++ main.py
@@ -25,7 +25,6 @@ class MainWindow(QMainWindow):
 
         # Create and connect controls to controller
         self._controller = Controller(self._model, self._scrollable_canvas.canvas)
-        # self._connect_controls()
 
     def _setup_ui(self):
         central_widget = QWidget()
@@ -51,27 +50,6 @@ class MainWindow(QMainWindow):
         window_width = min(self._canvas_width + 250, 1400)
         window_height = min(self._canvas_height + 100, 900)
         self.resize(window_width, window_height)
-
-    # def _connect_controls(self):
-    #     # Connect colour control
-    #     self._control_panel.colour_control.colour_changed.connect(
-    #         self._controller.set_line_colour
-    #     )
-        
-    #     # Connect brush size control
-    #     self._control_panel.brush_size_control.value_changed.connect(
-    #         self._controller.set_brush_size
-    #     )
-        
-    #     # Connect tool control
-    #     self._control_panel.tool_control.tool_changed.connect(
-    #         self._controller.set_tool
-    #     )
-        
-    #     # Connect clear canvas control
-    #     self._control_panel.clear_canvas_control.clear_clicked.connect(
-    #         self._controller.clear_canvas
-    #     )
 
     @property
     def canvas(self):
